4f31650ea6ae12d46e93f79f2b4d32249ad704b4
uriel <urielaracena@icloud.com>
2025-05-21 07:16:42 -0500

    project2

--- project2/config.py
+++ project2/config.py
@@ -2,5 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:aracena@localhost/salon_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Use DATABASE_URL for deployed environments or fallback to localhost for local dev
+    if os.environ.get('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:aracena@localhost/salon_db'
