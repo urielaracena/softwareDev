d5caffff9750e28cfa6784d4f43dc933bc6bcad2
uriel <urielaracena@icloud.com>
2025-05-21 07:05:29 -0500

    project2

--- project2/config.py
+++ project2/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:aracena@localhost/salon_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:aracena@localhost/salon_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
